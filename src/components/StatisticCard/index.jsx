fa526a5d12bc592eb0c5e8be3dccd627cbb06984
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-05-09 17:15:54 +0200

    Implement card

---
+++ src/components/StatisticCard/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import iconCalories from '../../assets/icon-calories.svg';
+import iconProtein from '../../assets/icon-protein.svg';
+import iconCarbs from '../../assets/icon-carbs.svg';
+import iconFat from '../../assets/icon-fat.svg';
+import styles from './index.module.scss';
+
+const getCardInfosBasedOnType = (type) => {
+  let iconSource,
+    statName,
+    statUnit = '';
+
+  switch (type) {
+    case 'calories':
+      iconSource = iconCalories;
+      statName = 'Calories';
+      statUnit = 'kCal';
+      break;
+    case 'protein':
+      iconSource = iconProtein;
+      statName = 'Proteines';
+      statUnit = 'g';
+      break;
+    case 'carbs':
+      iconSource = iconCarbs;
+      statName = 'Glucides';
+      statUnit = 'g';
+      break;
+    case 'fat':
+      iconSource = iconFat;
+      statName = 'Lipides';
+      statUnit = 'g';
+      break;
+  }
+  return { iconSource, statName, statUnit };
+};
+
+function StatisticCard({ type, value }) {
+  const { iconSource, statName, statUnit } = getCardInfosBasedOnType(type);
+
+  return (
+    <article className={styles.card}>
+      <img src={iconSource} />
+      <div className={styles.statistic}>
+        <p className={styles.statistic__value}>
+          {value}
+          {statUnit}
+        </p>
+        <p className={styles.statistic__name}>{statName}</p>
+      </div>
+    </article>
+  );
+}
+
+StatisticCard.propTypes = {
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
+export default StatisticCard;
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './index.module.scss';
+import StatisticCard from '../../components/StatisticCard';
 
 export default function Profile() {
   const { userId } = useParams();
 
   console.log(userId);
 
-  return <main className={styles.main}></main>;
+  return (
+    <div className={styles.profileContainer}>
+      <ul className={styles.statsListing}>
+        <li>
+          <StatisticCard type={'calories'} value={'3'} />
+        </li>
+        <li>
+          <StatisticCard type={'protein'} value={'3'} />
+        </li>
+        <li>
+          <StatisticCard type={'carbs'} value={'3'} />
+        </li>
+        <li>
+          <StatisticCard type={'fat'} value={'3'} />
+        </li>
+      </ul>
+    </div>
+  );
 }
